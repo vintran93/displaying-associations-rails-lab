52c78c1fa42ba127be653a2aa3a0ff3ef06f24de
vincent <vincentuong@yahoo.com>
2021-11-13 14:28:43 -0600

    commit tree 84784c22127a444988cff3d2b5c4f97089da504c

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,8 +1,10 @@
 class SongsController < ApplicationController
   def index
+    @songs = Song.all
   end
 
   def show
+    @song = Song.find(params[:id])
   end
 
   def new
@@ -11,8 +13,6 @@ class SongsController < ApplicationController
 
   def create
     @song = Song.new(song_params)
-    @song.artist = Artist.find_or_create_by(artist_params)
-
     if @song.save
       redirect_to @song
     else
@@ -47,9 +47,5 @@ class SongsController < ApplicationController
 
   def song_params
     params.require(:song).permit(:title)
-  end
-
-  def artist_params
-    params.require(:artist).permit(:name)
   end
 end
